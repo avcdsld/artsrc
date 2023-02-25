d1055d3f27c392df66fefb4344802b8b8f72ec3e
avcdsld <avcdsld@gmail.com>
2023-02-26 01:46:31 +0900

    implement contracts

---
+++ test.sh
@@ -0,0 +1,24 @@
+flow transactions send ./cadence/transactions/setup.cdc --signer default
+
+flow transactions send ./cadence/test/mint.cdc --signer default
+
+flow transactions send ./cadence/transactions/mint.cdc \
+    p5js testTitle testDescription testImageIpfsCid testArtistName testCode \
+    --signer default
+
+flow transactions send ./cadence/transactions/mint_contract.cdc \
+    cadence testTitle testDescription testImageIpfsCid testArtistName \
+    0xf3fcd2c1a78f5eee ArtSource \
+    --signer default
+
+flow transactions send ./cadence/transactions/update.cdc \
+    2 testTitle2 testDescription2 testImageIpfsCid testArtistName testCode2 \
+    --signer default
+
+flow scripts execute ./cadence/scripts/get_nft_metadata.cdc f3fcd2c1a78f5eee 2
+
+flow scripts execute ./cadence/scripts/get_nft_metadata.cdc f3fcd2c1a78f5eee 3
+
+flow scripts execute ./cadence/scripts/get_user_nfts.cdc f3fcd2c1a78f5eee
+
+flow scripts execute ./cadence/scripts/get_showcase_nfts.cdc 0 8
